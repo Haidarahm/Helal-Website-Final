cca3dc9a67c327c2919de96e2b9f1a8374861b18
haidaraahmad-prog <haidara.ahmad@tikit.ae>
2026-01-21 14:52:22 +0500

    feat: Add API functions for fetching broadcasts by language/page and by ID.

--- src/apis/broadcasts.js
+++ src/apis/broadcasts.js
@@ -3,9 +3,7 @@ import api from "./config.js";
 // Get broadcasts by language and page
 export const getBroadcasts = async (lang = "ar", page = 1, perPage = 6) => {
     try {
-        // Note: This endpoint might not exist yet, we will mock it in the store if it fails
-        // or if you want to use local mock data, we can define it in the store directly.
-        const response = await api.get(`/broadcasts`, {
+        const response = await api.get(`/podcasts/all`, {
             params: {
                 lang,
                 page,
@@ -20,7 +18,7 @@ export const getBroadcasts = async (lang = "ar", page = 1, perPage = 6) => {
 
 export const getBroadcastById = async (id, lang = "ar") => {
     try {
-        const response = await api.get(`/broadcasts/${id}`, { params: { lang } });
+        const response = await api.get(`/podcasts/show/${id}`, { params: { lang } });
         return response.data;
     } catch (error) {
         throw error;
